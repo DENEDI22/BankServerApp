98e40d56b7b767bd08bf79a8d39fbe38c573bf90
Danylo Liubymyi <denedi2004@gmail.com>
2022-11-10 17:45:58 +0100

    Exchange tables file added

---
+++ BankServerApp/BankClasses/ExchangeTables.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml;
+
+namespace BankServerApp;
+
+public class ExchangeTables
+{
+    private Dictionary<Currencies, decimal> comparedToUAHExchangeRate = new Dictionary<Currencies, decimal>();
+
+    public decimal GetFromExchangeRateTable(Currencies _innitialCurrency, Currencies _returnCurrency, decimal amount)
+    {
+        var tmp = amount / comparedToUAHExchangeRate[_innitialCurrency];
+        return tmp * comparedToUAHExchangeRate[_returnCurrency];
+    }
+
+    private static Currencies? ParseCurrency(string _string)
+    {
+        switch (_string)
+        {
+            case "usd":
+                return Currencies.USD;
+            case "eur":
+                return Currencies.EUR;
+            case "uah":
+                return Currencies.UAH;
+            case "gbp":
+                return Currencies.GBP;
+            case "pln":
+                return Currencies.PLN;
+            default:
+                return null;
+        }
+    }
+
+    public void LoadRates()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+        foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+        {
+            var parsedCurrency = ParseCurrency(node.Attributes["cc"].Value);
+            if (parsedCurrency != null)
+            {
+                comparedToUAHExchangeRate.Add((Currencies)parsedCurrency, decimal.Parse(node.Attributes["rate"].Value));
+            }
+        }
+    }
+}
